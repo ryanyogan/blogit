998979c05f8c28e411def776c0123a43f7440408
Ryan Yogan <ryanyogan@gmail.com>
2016-05-06 15:05:22 -0500

    MVP of comments complete, requires refacorting of duplication

--- test/controllers/comment_controller_test.exs
+++ test/controllers/comment_controller_test.exs
@@ -16,8 +16,14 @@ defmodule Blogit.CommentControllerTest do
     {:ok, conn: conn, user: user, post: post, comment: comment}
   end
 
+  defp login_user(conn, user) do
+    post conn, session_path(conn, :create), user: %{username: user.username,
+                                                    password: user.password}
+  end
+
   test "creates resource and redirects when data is valid", %{conn: conn, post: post} do
-    _conn = post conn, post_comment_path(conn, :create, post), comment: @valid_attrs
+    conn = post conn, post_comment_path(conn, :create, post), comment: @valid_attrs
+    assert redirected_to(conn) == user_post_path(conn, :show, post.user, post)
     assert Repo.get_by(assoc(post, :comments), @valid_attrs)
   end
 
@@ -26,9 +32,42 @@ defmodule Blogit.CommentControllerTest do
     assert html_response(conn, 200) =~ "Oops, something went wrong"
   end
 
-  test "deletes the comment", %{conn: conn, post: post, comment: comment} do
-    conn = delete(conn, post_comment_path(conn, :delete, post, comment))
+  test "updates chosen resource and redirects when data is valid and logged in as author",
+  %{conn: conn, user: user, post: post, comment: comment} do
+    conn = login_user(conn, user) |> put(post_comment_path(conn, :update, post, comment),
+      comment: %{"approved" => true})
+    assert redirected_to(conn) == user_post_path(conn, :show, user, post)
+    assert Repo.get_by(Comment, %{id: comment.id, approved: true})
+  end
+
+  test "deletes the comment when logged in as an authorized user",
+  %{conn: conn, user: user, post: post, comment: comment} do
+    conn = login_user(conn, user) |> delete(post_comment_path(conn, :delete,
+          post, comment))
     assert redirected_to(conn) == user_post_path(conn, :show, post.user, post)
     refute Repo.get(Comment, comment.id)
+  end
+
+  test "does not delete the comment when not logged in as an authorized user",
+  %{conn: conn, post: post, comment: comment} do
+    conn = delete(conn, post_comment_path(conn, :delete, post, comment))
+    assert redirected_to(conn) == page_path(conn, :index)
+    assert Repo.get(Comment, comment.id)
+  end
+
+  test "updates chosen resource and redirects when data is valid, logged in as author",
+  %{conn: conn, user: user, post: post, comment: comment} do
+    conn = login_user(conn, user) |> put(post_comment_path(conn, :update, post, comment),
+      comment: %{"approved" => true})
+    assert redirected_to(conn) == user_post_path(conn, :show, user, post)
+    assert Repo.get_by(Comment, %{id: comment.id, approved: true})
+  end
+
+  test "does not update the comment when not logged in as an authorized user",
+  %{conn: conn, post: post, comment: comment} do
+    conn = put(conn, post_comment_path(conn, :update, post, comment),
+      comment: %{"approved" => true})
+    assert redirected_to(conn) == page_path(conn, :index)
+    refute Repo.get_by(Comment, %{id: comment.id, approved: true})
   end
 end
--- web/controllers/comment_controller.ex
+++ web/controllers/comment_controller.ex
@@ -5,6 +5,31 @@ defmodule Blogit.CommentController do
   alias Blogit.Post
 
   plug :scrub_params, "comment" when action in [:create, :update]
+  plug :set_post_and_authorize_user when action in [:update, :delete]
+
+  defp set_post(conn) do
+    post = Repo.get!(Post, conn.params["post_id"])
+      |> Repo.preload(:user)
+    assign(conn, :post, post)
+  end
+
+  defp set_post_and_authorize_user(conn, _opts) do
+    conn = set_post(conn)
+    if is_authorized_user?(conn) do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You are not authorized to modify that comment!")
+      |> redirect(to: page_path(conn, :index))
+      |> halt
+    end
+  end
+
+  defp is_authorized_user?(conn) do
+    user = get_session(conn, :current_user)
+    post = conn.assigns[:post]
+    (user && (user.id == post.user_id || Blogit.RoleChecker.is_admin?(user)))
+  end
 
   def create(conn, %{"comment" => comment_params, "post_id" => post_id}) do
     post = Repo.get!(Post, post_id) |> Repo.preload([:user, :comments])
@@ -23,7 +48,23 @@ defmodule Blogit.CommentController do
         )
     end
   end
-  def update(conn, _), do: conn
+
+  def update(conn, %{"id" => id, "post_id" => post_id, "comment" => comment_params}) do
+    post = Repo.get!(Post, post_id) |> Repo.preload(:user)
+    comment = Repo.get!(Comment, id)
+    changeset = Comment.changeset(comment, comment_params)
+
+    case Repo.update(changeset) do
+      {:ok, _} ->
+        conn
+        |> put_flash(:info, "Comment updated successfully.")
+        |> redirect(to: user_post_path(conn, :show, post.user, post))
+      {:error, _} ->
+        conn
+        |> put_flash(:info, "Failed to update comment!")
+        |> redirect(to: user_post_path(conn, :show, post.user, post))
+    end
+  end
 
   def delete(conn, %{"id" => id, "post_id" => post_id}) do
     post = Repo.get!(Post, post_id) |> Repo.preload(:user)
